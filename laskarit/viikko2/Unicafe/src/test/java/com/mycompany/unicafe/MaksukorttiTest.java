6e7440e5f14d0b209139b8b680e483d5716d6fad
Antti Mattila <antti.mattila@reaktor.fi>
2018-03-20 22:30:37 +0200

    Kortti tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -1,8 +1,11 @@
 package com.mycompany.unicafe;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class MaksukorttiTest {
 
@@ -15,6 +18,29 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void saldoOikeinAluksi() {
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void rahanLisaaminenKasvattaaSaldoa() {
+        kortti.lataaRahaa(10);
+        assertEquals(20, kortti.saldo());
+    }
+
+    @Test
+    public void rahaaVaheneeOnnistuneestiKunSaldoaRiittaa() {
+        assertTrue(kortti.otaRahaa(2));
+        assertEquals(8, kortti.saldo());
+    }
+
+    @Test
+    public void rahanOttoEpaonnistuuEikaSaldoMuutuKunRahaEiRiita() {
+        assertFalse(kortti.otaRahaa(11));
+        assertEquals(10, kortti.saldo());
     }
 }
