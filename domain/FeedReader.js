abbf5ada8e518dfd2114424ad0a1edf7488696da
Antti Mattila <antti.mattila@reaktor.fi>
2018-04-27 03:18:53 +0300

    Generate Java(script)Docs

--- domain/FeedReader.js
+++ domain/FeedReader.js
@@ -5,12 +5,18 @@ module.exports = {
 const _ = require('lodash')
 const datastore = require('./datastore.js')
 
+/**
+ * Get list of feeds grouped by categories
+ */
 function feedList() {
   return _(datastore.getFeeds())
     .groupBy('category')
     .value()
 }
 
+/**
+ * Add feed object to datastore
+ */
 function addFeed(feed) {
   datastore.addFeed(feed)
 }
